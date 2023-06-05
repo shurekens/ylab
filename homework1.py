23ca8c49597c695f00a8dfe888f89f706f10b944
shurekens <43280729+shurekens@users.noreply.github.com>
2023-06-05 20:30:06 +0400

    Update homework1.py
    Corrected a typo in Task 1, added Task 2

--- homework1.py
+++ homework1.py
@@ -1,9 +1,10 @@
+#Task №1
 def domain_name(url):
   first_dot = url.find(".") + 1
   second_dot = url.find(".", first_dot)
   slashes = url.find("//") + 2
   if "www." in url:
-    return(url[first_dot : decond_dot])
+    return(url[first_dot : second_dot])
   else:
     return(url[slashes : first_dot - 1])
   
@@ -12,3 +13,14 @@ assert domain_name("http://google.co.jp") == "google"
 assert domain_name("www.xakep.ru") == "xakep"
 assert domain_name("https://youtube.com") == "youtube"
 
+#Task №2
+def int32_to_ip(int32):
+  a = int32 // 16777216
+  b = (int32 - (a * 16777216)) // 65536
+  c = (int32 - (a * 16777216 + b * 65536)) // 256
+  d = (int32 - (a * 16777216 + b * 65536 + c * 256))
+  return(f"{a}.{b}.{c}.{d}")
+
+assert int32_to_ip(2154959208) == "128.114.17.104"
+assert int32_to_ip(0) == "0.0.0.0"
+assert int32_to_ip(2149583361) == "128.32.10.1"
