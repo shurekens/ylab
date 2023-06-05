8ea330ecdf532d132a941614da2fa4d5a890415a
shurekens <43280729+shurekens@users.noreply.github.com>
2023-06-05 11:17:07 +0400

    Update homework1.py
    Added first task of the first homework

--- homework1.py
+++ homework1.py
@@ -1,1 +1,14 @@
+def domain_name(url):
+  first_dot = url.find(".") + 1
+  second_dot = url.find(".", first_dot)
+  slashes = url.find("//") + 2
+  if "www." in url:
+    return(url[first_dot : decond_dot])
+  else:
+    return(url[slashes : first_dot - 1])
+  
+assert domain_name("http://google.com") == "google"
+assert domain_name("http://google.co.jp") == "google"
+assert domain_name("www.xakep.ru") == "xakep"
+assert domain_name("https://youtube.com") == "youtube"
 
