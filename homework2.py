25bb1857a6d6febf420029f0b36e229cf6123926
shurekens <43280729+shurekens@users.noreply.github.com>
2023-07-04 22:27:07 +0300

    Update homework2.py
    Added Task #1

--- homework2.py
+++ homework2.py
@@ -1,1 +1,20 @@
+#Task №1 
+class CyclicIterator:
+    def __init__(self, container):
+        self.container = container
+        self.iterator = iter(container)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            try:
+                print(next(self.iterator))
+            except StopIteration:
+                self.iterator = iter(self.container)
+
+cyclic_iterator = CyclicIterator(range(3))
+for i in cyclic_iterator:
+    print(i)
 
