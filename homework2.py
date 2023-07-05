a259eaf115f992940b3f8f3b8380ad35ee5ceede
shurekens <43280729+shurekens@users.noreply.github.com>
2023-07-05 22:23:24 +0300

    Update homework2.py
    Added Task #2

--- homework2.py
+++ homework2.py
@@ -18,3 +18,28 @@ cyclic_iterator = CyclicIterator(range(3))
 for i in cyclic_iterator:
     print(i)
 
+#Task №2
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Generator, List, Tuple
+
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+    def schedule(self) -> Generator[datetime, None, None]:
+        list = []
+        for element in self.dates:
+            date = element[0]
+            while date <= element[1]:
+                yield(date)
+                date += timedelta(1)
+
+m = Movie('sw', [
+  (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+  (datetime(2020, 1, 15), datetime(2020, 2, 7))
+])
+
+for d in m.schedule():
+    print(d)
